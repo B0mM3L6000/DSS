d31a39058d05942982a2b8109a061e9f8fe741f5
Martin <38504660+B0mM3L6000@users.noreply.github.com>
2018-05-11 18:02:44 +0200

    Add files via upload

---
+++ casestudy.R
@@ -0,0 +1,30 @@
+setwd("D:/Projekte/DSS/DSS/CaseStudy")
+
+#einlesen der csv files
+
+data <- "2007.csv"
+airports <- "airports.csv"
+carriers <- "carriers.csv"
+planes <- "plane-data.csv"
+
+#einlesen in dataframes
+
+datadf <- read.csv(data, sep=",", header=TRUE, encoding="UTF-8")
+airportsdf <- read.csv(airports, sep=",", header=TRUE, encoding="UTF-8")
+carriersdf <- read.csv(carriers, sep=",", header=TRUE, encoding="UTF-8")
+planesdf <- read.csv(planes, sep=",", header=TRUE, encoding="UTF-8")
+
+
+Vars <- colnames(datadf)
+Namen <- c("Year", "FlightNum")
+datadf <- datadf[, !(Vars %in% Namen)]
+
+Vars <- colnames(planesdf)
+Namen <- c("type", "issue_date", "status")
+planesdf <- planesdf[, !(Vars %in% Namen)]
+
+datadf <- merge(datadf, planesdf, by.x=c("TailNum"), by.y=c("tailnum"))
+datadf
+
+write.table(datadf, "data.csv", sep=",",row.names = F)
+
